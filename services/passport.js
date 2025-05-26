ad3cba5234b38fae8860cb3fff12fc863ea59978
zhivkoGrchev <z.grcev@gmail.com>
2025-05-26 10:24:43 +0200

    proxy-true

--- services/passport.js
+++ services/passport.js
@@ -17,8 +17,10 @@ passport.use(
   new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-  }, (accessToken, refreshToken, profile, done) => {
+    callbackURL: '/auth/google/callback',
+    proxy: true
+  }, 
+    (accessToken, refreshToken, profile, done) => {
         User.findOne({ googleId: profile.id }).then((existingUser) => {
           if (existingUser) {
             //we already have a record with the given profile ID
